2e8e6dac4034ad2b9891aec3501b2b3bba826a45
xivk <ben@itinero.tech>
2020-10-11 21:13:48 +0200

    Added cycling network basic.

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ import { UrlHash } from './components/url-hash/UrlHash';
 const urlState = UrlHash.read();
 
 // parse the map state.
-const mapState: { 
+const mapState: {
     center: LngLatLike
     zoom: number
 } = {
@@ -41,32 +41,93 @@ map.on("load", e => {
     }
     if (typeof urlState.o !== "undefined") {
         const parts = urlState.o.split(",");
-    
+
         if (parts.length === 2) {
             rc.setOrigin([parseFloat(parts[0]), parseFloat(parts[1])]);
         }
     }
     if (typeof urlState.d !== "undefined") {
         const parts = urlState.d.split(",");
-    
+
         if (parts.length === 2) {
             rc.setDestination([parseFloat(parts[0]), parseFloat(parts[1])]);
         }
     }
 
-    function updateMapUrlState () {
+    function updateMapUrlState() {
         const center = map.getCenter();
         urlState.map = `${map.getZoom().toFixed(2)}/${center.lng.toFixed(5)}/${center.lat.toFixed(5)}`;
-    
+
         UrlHash.write(urlState);
     }
     if (typeof urlState.map === "undefined") {
         updateMapUrlState();
     }
-    
+
     map.on("moveend", e => {
         updateMapUrlState();
     });
+
+    // get lowest label and road.
+    var style = map.getStyle();
+    var lowestRoad = undefined;
+    var lowestLabel = undefined;
+    var lowestSymbol = undefined;
+    for (var l = 0; l < style.layers.length; l++) {
+        var layer = style.layers[l];
+
+        if (layer && layer["source-layer"] === "transportation") {
+            if (!lowestRoad) {
+                lowestRoad = layer.id;
+            }
+        }
+
+        if (layer && layer["source-layer"] === "transportation_name") {
+            if (!lowestLabel) {
+                lowestLabel = layer.id;
+            }
+        }
+
+        if (layer && layer.type == "symbol") {
+            if (!lowestSymbol) {
+                lowestSymbol = layer.id;
+            }
+        }
+    }
+
+    map.addSource('cyclenetworks-tiles', {
+        type: 'vector',
+        url: 'https://routing.anyways.eu/vector-tiles/cyclenetworks/mvt.json'
+    });
+
+    map.addLayer({
+        "id": "cyclenetworks",
+        "type": "line",
+        "source": "cyclenetworks-tiles",
+        "source-layer": "cyclenetwork",
+        "layout": {
+            "line-join": "round",
+            "line-cap": "round"
+        },
+        "paint": {
+            "line-color": "#ff0000",
+            "line-width": [
+                'interpolate', ['linear'], ['zoom'],
+                10, 1,
+                13, 2,
+                16, 4
+            ],
+            "line-opacity": 1
+        },
+        "filter": [
+            "all",
+            [
+                "==",
+                "$type",
+                "LineString"
+            ]
+        ]
+    }, lowestLabel);
 })
 
 rc.on('origin', c => {
